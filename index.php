295dad93230eb093dbc394f6130efc140119613a
Jeff <imjeff@qq.com>
2024-07-21 14:49:49 +0800

    v1.1.0
    - 增加独立“搜索模板”
    - CSS 样式移至 style.css 文件
    - 部分样式完善

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Writing 是一款极致极简写作主题，支持自定义背景、自定义菜单、暗黑模式、自适应，保留搜索及评论功能；内置文章归档模板。主题无 JS、CSS 文件载入，对程序极简优化，仅9个文件约50kb。<br/>
+ * Writing 是一款 WordPress & Typecho 极致极简写作主题，支持自定义背景、自定义菜单、暗黑模式、自适应，保留搜索及评论功能；内置文章归档模板和搜索模板。主题无 JS 和图片文件载入。<br/>
  * 发布页：<a href="https://yayu.net/projects/typecho-writing" target="_blank">https://yayu.net/projects/typecho-writing</a>
  *
  * @package Writing
  * @author Jeff Chen
- * @version 1.0.0
+ * @version 1.1.0
  * @link https://yayu.net/
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
@@ -14,8 +14,7 @@ $this->need('header.php'); ?>
 <h1 class="title"><a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a></h1>
 <div class="home intro"><?php $this->options->description() ?></div>
 <?php }; if ($this->is('archive')) { ?>
-<h1 class="title"><?php $this->archiveTitle(['category' => _t('%s'),'search' => _t('搜索结果'),'tag' => _t('标签：%s'),'author' => _t('作者：%s')], '', ''); ?></h1>
-<div class="intro"><?php if ( $this->is('search') ) { ?>关键词：<?php echo $this->archiveTitle('','',''); ?><?php } else { ?><?php echo $this->getDescription(); ?><?php } ?></div>
+<h1 class="title"><?php $this->archiveTitle(['category' => _t('%s'),'tag' => _t('标签：%s'),'author' => _t('作者：%s')], '', ''); ?></h1>
 <?php } ?>
 <?php if ($this->have()): ?>
 <?php while ($this->next()): ?>
@@ -31,5 +30,6 @@ $this->need('header.php'); ?>
 <div class="comlist"><br /><?php $this->need('comments.php'); ?></div>
 <?php }; if ( $this->is('archive') || $this->is('index') ) { ?>
 <div class="pages"><?php $this->pageLink('&nbsp;←&nbsp;'); ?><?php $this->pageLink('&nbsp;→&nbsp;','next'); ?></div>
-<?php }; endif; ?>
+<?php }; ?>
+<?php else : ?><article><em>空空如也 ...</em></article><?php endif; ?>
 <?php $this->need('footer.php'); ?>
